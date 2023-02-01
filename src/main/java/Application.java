f7ba7fe3306be1a7ff44d0a262f6790725f9642d
Michael Harris <mharrisjr01@gmail.com>
2023-02-01 23:21:03 +0000

    Completed Lab Combine Strings

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,5 @@
 public class Application {
-    /**
+    /***
      * This class contains a main method that allows you to manually test the CombineStrings lab functionality.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
